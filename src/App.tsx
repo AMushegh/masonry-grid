ea8d92b9b4a7810006de90007e386e0a8dd762e4
Mushegh Andrasyan <mushegh.andrasyan@gmail.com>
2025-10-09 19:19:37 +0400

    adds suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
+import { Suspense } from "react";
 
 export const App = () => {
   return (
     <>
-      <RouterProvider router={router} />
+      <Suspense fallback={null}>
+        <RouterProvider router={router} />
+      </Suspense>
     </>
   );
 };
